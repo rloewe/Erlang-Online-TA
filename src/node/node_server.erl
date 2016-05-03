687b8ae6b7a1b900f8b435332d4564c8929effc7
René Løwe Jacobsen <rlj@rloewe.net>
2016-05-03 14:10:54 +0200

    Started queue_assignment_job

--- src/node/node_server.erl
+++ src/node/node_server.erl
@@ -3,7 +3,7 @@
 -import (master_server, [connect_to/2]).
 -import (config_parser, [parse_config/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
--export([start/1]).
+-export([start/1, queue_assignment_job/4]).
 
 start(_ConfigFile) ->
     %TODO add parse config
@@ -12,11 +12,14 @@ start(_ConfigFile) ->
     net_kernel:connect_node(MasterNode),
     gen_server:start_link({local, ?MODULE}, ?MODULE, [MasterNode,Specs], []).
 
+queue_assignment_job(Node, AssignmentID, Files, SessionToken) ->
+    gen_server:call({?MODULE, Node}, {queue_job, {AssignmentID, Files, SessionToken}}).
+
 init([MasterNode,Specs]) ->
     %master:connect_to(node()),
     case connect_to(node(),Specs) of
         ok ->
-            Queue = none,
+            Queue = queue:new(),
             Assignments = dict:new(),
             CurrentJobs = [],
             {ok, {Queue,Assignments,CurrentJobs,MasterNode}};
@@ -30,7 +33,23 @@ handle_cast(_Message, State) ->
     {noreply, State}.
 
 
-
+handle_call(
+  {queue_job, Assignment},
+  _From,
+  {Queue, Assignments, CurrentJobs, MasterNode}
+ ) ->
+    %TODO handle assignment id
+    %TODO fix magic constant
+    if length(CurrentJobs) < 2 ->
+           {AssignmentID, Files, SessionToken} = Assignment,
+           NewCurrentJobs = [Assignment | CurrentJobs],
+           io:format("Queue: ~p ~nCurrentJobs: ~p", [Queue, NewCurrentJobs]),
+           {reply, started, {Queue, Assignments, NewCurrentJobs, MasterNode}};
+       true ->
+           NewQueue = queue:in(Assignment, Queue),
+           io:format("Queue: ~p ~nCurrentJobs: ~p", [NewQueue, CurrentJobs]),
+           {reply, ok, {NewQueue, Assignments, CurrentJobs, MasterNode}}
+    end;
 handle_call(_Message, _From, State) ->
     {reply, error, State}.
 
