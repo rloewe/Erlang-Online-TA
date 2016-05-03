8f249dbb34e3e3bbd2cdbce174d223729747f538
simon <smaibom@gmail.com>
2016-05-03 10:12:05 +0200

    node server init

--- src/node/node_server.erl
+++ src/node/node_server.erl
@@ -1,20 +1,36 @@
--module(master_server).
+-module(node_server).
 -behaviour(gen_server).
 
-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-export([start/1]).
 
-start() ->
-    gen_server:start_link({local, node}, ?MODULE, [], []).
+start(_ConfigFile) ->
+    %TODO add parse config
+    {Cookie,MasterNode} = {test,'master@10.129.1.181'},
+    erlang:set_cookie(Cookie),
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [MasterNode], []).
 
-init([]) ->
-    {ok, []}.
+init([MasterNode]) ->
+    %master:connect_to(node()),
+    case gen_server:call({add_node,node(),none}) of
+        ok -> 
+            Queue = none,
+            Assignments = dict:dict(),
+            CurrentJobs = [],
+            {ok, {Queue,Assignments,CurrentJobs,MasterNode}};
+        _ ->
+            {stop,nocon}
+    end.
 
-handle_cast(Message, State) ->
+
+handle_cast(_Message, State) ->
     {noreply, State}.
 
-handle_call(Message, From, State) ->
+
+
+handle_call(_Message, _From, State) ->
     {reply, error, State}.
 
-handle_info(Message, State) -> {noreply, State}.
-terminate(Reason, State) -> ok.
-code_change(OldVersion, State, Extra) -> {ok, State}.
+handle_info(_Message, State) -> {noreply, State}.
+terminate(_Reason, _State) -> ok.
+code_change(_OldVersion, State, _Extra) -> {ok, State}.
