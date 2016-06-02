b085c80e6a04ce3977e15b8baf301633ca66c7b3
simon <smaibom@gmail.com>
2016-06-02 13:42:00 +0200

    assignment parser update

--- src/lib/assignment_parser.erl
+++ src/lib/assignment_parser.erl
@@ -2,7 +2,9 @@
 
 -export ([parse_assignment/1,parse/1]).
 
-%A bad implementation of a parser for assignment parser
+%Parser for assignment parser, a valid config file seperates each entry to be parsed
+%seperated by newlines, each entry follows: Entry = value, in the case of entries
+%with multiple values it is seperated by commas on 1 line
 
 
 parse_assignment({AssignmentID,Module}) ->
@@ -11,15 +13,18 @@ parse_assignment({AssignmentID,Module}) ->
 
 parse(Path) ->
     %TODO check for default vals
+    %TODO send back a proper missing req error
     case file:read_file(Path) of
          {ok, Binary} ->
             Content = binary_to_list(Binary),
             Lines = string:tokens(Content, "\n"),
             Dict = populate_dict(Lines, dict:new()),
-            Required = dict:is_key("runorder", Dict) and dict:is_key("module", Dict),
+            Required = dict:is_key("runorder", Dict) and dict:is_key("module", Dict)
+            and dict:is_key("assignmentid",Dict),
             if 
                 Required -> 
-                    {ok, Dict};
+                    NewDict = add_defaults(Dict),
+                    {ok, NewDict};
                 true ->
                     {error,missing_reqs}
             end;
@@ -57,4 +62,24 @@ parse_runorder_files([Value | Values], Parsed) ->
             parse_runorder_files(Values,[{unsafe,lists:nth(1,Elems)} | Parsed]);        
         _ ->
             parse_runorder_files(Values,[{safe,Elem} | Parsed])
+    end.
+
+add_defaults(Dict) ->
+    case file:read_file("./default.conf") of
+        {ok, Binary} ->
+            Content = binary_to_list(Binary),
+            Lines = string:tokens(Content, "\n="),
+            add_default_values(Lines,Dict);
+         Error -> Error
+    end.
+
+add_default_values([],Dict) ->
+    Dict;
+add_default_values([Entry,Value | Rest],Dict) ->
+    case dict:is_key(Entry,Dict) of
+        true ->
+            add_default_values(Rest,Dict);
+        false ->
+            NewDict = dict:store(Entry,Value,Dict),
+            add_default_values(Rest,NewDict)
     end.
