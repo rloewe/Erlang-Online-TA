532f16b2ca5a7163b790517ec0f2e6a6303c5fa1
René Løwe Jacobsen <rlj@rloewe.net>
2016-06-05 15:48:16 +0200

    Better web

--- src/web/src/assignment_handler.erl
+++ src/web/src/assignment_handler.erl
@@ -1,6 +1,6 @@
 -module(assignment_handler).
 -behaviour(cowboy_http_handler).
--import(master_server, [add_assignment/2]).
+-import(master_server, [add_assignment/3]).
 
 -export([init/3]).
 -export([handle/2]).
@@ -16,13 +16,31 @@ handle(Req, State=#state{}) ->
     case cowboy_req:parse_header(<<"content-type">>, Req) of
         {ok, {<<"multipart">>, <<"form-data">>, _}, Req2} ->
             {ok, Input, _Req3} = formdata:multipart(Req2),
-            {ok, Config} = dict:find("config", Input),
-            add_assignment(Config, 'master@127.0.0.1'),
-            {ok, Req4} = cowboy_req:reply(200,
-                [{<<"content-type">>, <<"text/html">>}],
-                <<"Assignment uploaded">>,
-                Req),
-            {ok, Req4, State};
+            {ok, [{file, _Filename, Content}]} = dict:find(<<"config">>, Input),
+            {ok, Files} = dict:find(<<"files">>, Input),
+            Response = add_assignment(Content,
+                           'master@127.0.0.1',
+                           lists:map(fun ({file, Filename, FileContent}) ->
+                                             {binary:bin_to_list(Filename),
+                                              FileContent}
+                                     end,
+                                     Files)),
+            case Response of
+                {ok, _ID} ->
+                    io:format("Good"),
+                    {ok, Req4} = cowboy_req:reply(200,
+                        [{<<"content-type">>, <<"text/html">>}],
+                        <<"Assignment uploaded">>,
+                        Req),
+                    {ok, Req4, State};
+                {error, _Error} ->
+
+                    {ok, Req4} = cowboy_req:reply(200,
+                        [{<<"content-type">>, <<"text/html">>}],
+                        <<"Error in assignment">>,
+                        Req),
+                    {ok, Req4, State}
+            end;
         {ok, _, _Req2} ->
             {ok, Req3} = cowboy_req:reply(200,
                 [{<<"content-type">>, <<"text/html">>}],
--- src/web/src/formdata.erl
+++ src/web/src/formdata.erl
@@ -11,10 +11,10 @@ multipartWithDict(Req, Dict) ->
             {Req4, UpdatedDict} = case cow_multipart:form_data(Headers) of
                        {data, FieldName} ->
                            {ok, Body, Req3} = cowboy_req:part_body(Req2),
-                           {Req3, dict:store(FieldName, {input, Body}, Dict)};
+                           {Req3, dict:append(FieldName, {input, Body}, Dict)};
                        {file, FieldName, Filename, _CType, _CTransferEncoding} ->
                            {ok, FileContent, Req3} = stream_file(Req2, <<>>),
-                           {Req3, dict:store(FieldName, {file, Filename, FileContent}, Dict)}
+                           {Req3, dict:append(FieldName, {file, Filename, FileContent}, Dict)}
                    end,
             multipartWithDict(Req4, UpdatedDict);
         {done, Req2} ->
