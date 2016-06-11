90b241465414b1f8b593aae5cd35b1972e257620
simon <smaibom@gmail.com>
2016-06-11 11:19:56 +0200

    Changing some monitor of nodes

--- src/lib/helper_functions.erl
+++ src/lib/helper_functions.erl
@@ -1,15 +1,15 @@
 -module (helper_functions).
 
--export ([create_dirs/1,save_files/2]).
+-export ([create_dirs/1,save_files/2,gen_directory_string/1]).
 
 
 create_dirs([]) ->
     ok;
 create_dirs([Path | Paths]) ->
     case file:make_dir(Path) of
-        Pat when Pat =:= ok; Pat =:= {error,eexist} ->        
+        Pat when Pat =:= ok; Pat =:= {error,eexist} ->
             create_dirs(Paths);
-        E -> 
+        E ->
             {error,E}
     end.
 
@@ -19,3 +19,11 @@ save_files([],_) ->
 save_files([{FileName,File} | Rest ],Path) ->
     file:write_file(Path++FileName,File),
     save_files(Rest,Path).
+
+
+gen_directory_string(Size) ->
+    Allowed = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+    NumberOfChars = length(Allowed),
+    FoldFun = fun(_,Accum) ->
+        [lists:nth(random:uniform(NumberOfChars),Allowed)] ++ Accum end,
+    lists:foldl(FoldFun,[],lists:seq(1,Size)).
