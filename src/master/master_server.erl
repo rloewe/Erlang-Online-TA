0bd0b374ee6e7f2fdad63be50f08a6c7a5c3c8be
René Løwe Jacobsen <rlj@rloewe.net>
2016-06-11 22:47:39 +0200

    tihi debug

--- src/master/master_server.erl
+++ src/master/master_server.erl
@@ -192,6 +192,7 @@ handle_call({update_job,SessionToken,NewStatus}, _From, State) ->
                     %TODO magic with files and return call to end user
 
                     io:format("Master server job finished ~p \n",[ReturnVal]),
+                    io:format("Dict has: ~p\n", [dict:fetch(SessionToken,State#masterState.sessions)]),
                     {_,_,DirID} = dict:fetch(SessionToken,State#masterState.sessions),
                     helper_functions:delete_dir("./Handins/" ++ DirID),
                     NewSessions = dict:erase(SessionToken,State#masterState.sessions),
