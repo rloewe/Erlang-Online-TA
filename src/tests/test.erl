6533c72fea3608dae66b6248a2155e4f8eef843c
simon <smaibom@gmail.com>
2016-06-17 19:17:19 +0200

    much test

--- src/tests/test.erl
+++ src/tests/test.erl
@@ -1,6 +1,7 @@
 -module (test).
 
--export ([add_docker/0,add_haskell/0,run_docker/0,run_haskell/0,run_n_docker/1,run_n_haskell/1]).
+-export ([add_docker/0,add_haskell/0,run_docker/0,run_haskell/0,run_n_docker/1,run_n_haskell/1,
+        no_assignment_test/0,no_module_test/0,bad_assignment_test/0,bad_module_test/0]).
 
 add_docker() ->
     Master = node(),
@@ -11,6 +12,8 @@ add_haskell() ->
     Master = node(),
     add_module(safehaskell, "./lib/safehaskell.beam", Master),
     add_assignment("/root/assignment2/assignment.conf",["/root/assignment2/Test.hs"],Master).
+
+
 
 add_module(Name,Path,Master) ->
     {ok,Binary} = file:read_file(Path),
@@ -51,3 +54,22 @@ run_n_haskell(N) ->
     {ok,Binary} = file:read_file("/root/ass2handin/Matematik.hs"),
     master_server:send_handin("hello2",[{"Matematik.hs",Binary}],node()),
     run_n_haskell(N-1).
+
+
+
+%Tests if stuff is not added yet
+no_assignment_test() ->
+    {error, no_assignment_id} = run_haskell().
+
+no_module_test() ->
+    Master = node(),
+    add_assignment("/root/assignment2/assignment.conf",["/root/assignment2/Test.hs"],Master).
+
+bad_module_test() ->
+    master_server:add_module(test,<<"BAD MODULE">>,node()).
+
+%Test that will crash the server
+bad_assignment_test() ->
+    master_server:add_assignment(<<"Bad file">>,[],node()).
+
+
